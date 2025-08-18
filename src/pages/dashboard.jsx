4c70179bbfc9949231940502e7749f077c01cfd6
JahazielB <baldemorjahaziel73@gmail.com>
2025-08-18 15:58:25 +0800

    changed inventory management system text color to blue

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -5,7 +5,7 @@ export const Dashboard = ()=>{
     return <div className="flex gap-1 ">
         <Sidebar/>
         <div className="flex flex-col ml-[5%] md:mx-[9%] lg:mx-[15%] xl:mx-[10%] max-md:gap-4 gap-4 p-4">
-            <div className="text-center text-white font-bold text-[20px] xl:text-[30px] ml-10">
+            <div className="text-center text-blue-600 font-bold text-[20px] xl:text-[30px] ml-10">
                 <h1>INVENTORY MANAGEMENT SYSTEM</h1>
             </div>
             <p className="text-white text-[15px] xl:text-[20px] font-bold">Hey Administator - <span className="font-normal text-[13px] xl:text-[16px]">here's what's happening today</span></p>
--- src/pages/inventoryPage.jsx
+++ src/pages/inventoryPage.jsx
@@ -10,7 +10,7 @@ export const InventoryPage = ()=>{
     return <div className="flex gap-1">
         <Sidebar/>
         <div className="flex flex-col ml-[5%] md:mx-[9%] lg:mx-[15%] xl:mx-[10%] max-md:gap-4 gap-4 p-4"> 
-            <div className="text-center text-white font-bold text-[20px] xl:text-[30px] ml-34">
+            <div className="text-center text-blue-600 font-bold text-[20px] xl:text-[30px] ml-34">
                 <h1>INVENTORY MANAGEMENT SYSTEM</h1>
             </div>
             {/* <div className="flex justify-between">
