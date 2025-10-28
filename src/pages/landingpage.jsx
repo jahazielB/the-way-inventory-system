263e9d5fba232f0721f2eea7995efa9ce3593915
jahazielB <baldemorjahaziel73@gmail.com>
2025-10-28 16:30:10 +0800

    redeploy

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -22,7 +22,7 @@ const LandingPage = ()=>{
             const {error:otpError} = await supabase.auth.signInWithOtp({
                 email,
                 options:{
-                    emailRedirectTo: `https://192.168.68.101:5173/auth/callback`  
+                    emailRedirectTo: `https://the-way-inventory.netlify.app/auth/callback`  
                 }
             })
 
